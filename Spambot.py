e58eae1be547879d19cc6161d87fe6cb12dc185b
Code_Cravings <70434329+codecravings@users.noreply.github.com>
2020-08-29 21:26:39 +0800

    Add files via upload

---
+++ Spambot.py
@@ -0,0 +1,7 @@
+import pyautogui, time    #pip install pyautogui
+
+time.sleep(5)    # Set the timer to run the code
+for word in open("script", "r"):  #Location of script,lyrics or whatever loaded 'r' to read
+    pyautogui.typewrite(word) #This will write words in the line
+    pyautogui.press("enter") #This will change the line
+    
